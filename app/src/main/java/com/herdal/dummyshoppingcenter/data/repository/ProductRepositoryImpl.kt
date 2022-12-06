34ed8a19d61245a0af93123397f5cb59e5c06b70
herdal06 <h.erdal721@gmail.com>
2022-12-06 13:34:32 +0300

    Create getProducts method on ProductRepository

---
+++ app/src/main/java/com/herdal/dummyshoppingcenter/data/repository/ProductRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.herdal.dummyshoppingcenter.data.repository
+
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.herdal.dummyshoppingcenter.common.ProductDataSource
+import com.herdal.dummyshoppingcenter.common.ProductMapper
+import com.herdal.dummyshoppingcenter.domain.repository.ProductRepository
+import com.herdal.dummyshoppingcenter.domain.uimodel.ProductUiModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import timber.log.Timber
+import javax.inject.Inject
+
+class ProductRepositoryImpl @Inject constructor(
+    private val remote: ProductDataSource.Remote,
+    private val mapper: ProductMapper
+) : ProductRepository {
+    override suspend fun getProducts(): Flow<PagingData<ProductUiModel>> {
+        val domainProduct = remote.getProducts().map { pagingData ->
+            pagingData.map { remoteProduct ->
+                mapper.mapRemoteProductToDomain(remoteProduct)
+            }
+        }
+        Timber.d("$domainProduct")
+        return domainProduct
+    }
+}
---
+++ app/src/main/java/com/herdal/dummyshoppingcenter/domain/repository/ProductRepository.kt
@@ -0,0 +1,10 @@
+package com.herdal.dummyshoppingcenter.domain.repository
+
+import androidx.paging.PagingData
+import com.herdal.dummyshoppingcenter.domain.uimodel.ProductUiModel
+import kotlinx.coroutines.flow.Flow
+
+interface ProductRepository {
+
+    suspend fun getProducts(): Flow<PagingData<ProductUiModel>>
+}
