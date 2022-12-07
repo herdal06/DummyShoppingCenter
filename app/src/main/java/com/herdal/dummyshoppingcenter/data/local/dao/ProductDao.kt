5feb195b3342ab86843b29c5857ed9834343c5f8
herdal06 <h.erdal721@gmail.com>
2022-12-07 13:53:02 +0300

    Provide AppDatabase, Dao and ProductLocalDataSource

---
+++ app/src/main/java/com/herdal/dummyshoppingcenter/data/local/dao/ProductDao.kt
@@ -0,0 +1,18 @@
+package com.herdal.dummyshoppingcenter.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.herdal.dummyshoppingcenter.data.local.entity.product.ProductEntity
+import com.herdal.dummyshoppingcenter.domain.uimodel.ProductUiModel
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ProductDao {
+
+    @Insert
+    suspend fun insert(productEntity: ProductEntity)
+
+    @Query("SELECT * FROM products")
+    fun getAll(): Flow<List<ProductUiModel>>
+}
