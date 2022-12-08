3c8f41ec06012366925e1a1645d57968e4760344
herdal06 <h.erdal721@gmail.com>
2022-12-08 11:46:44 +0300

    Setup bottom navigation view

--- app/src/main/java/com/herdal/dummyshoppingcenter/ui/MainActivity.kt
+++ app/src/main/java/com/herdal/dummyshoppingcenter/ui/MainActivity.kt
@@ -2,13 +2,43 @@ package com.herdal.dummyshoppingcenter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.herdal.dummyshoppingcenter.R
+import com.herdal.dummyshoppingcenter.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        supportActionBar?.hide()
+        setContentView(view)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.favoriteProductsFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp()
     }
 }
