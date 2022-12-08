0e999bd5a31f18470447cb98a713da0b17b64c96
herdal06 <h.erdal721@gmail.com>
2022-12-08 10:14:48 +0300

    Set OnConflictStrategy to insert to db method

--- app/src/main/java/com/herdal/dummyshoppingcenter/data/local/dao/ProductDao.kt
+++ app/src/main/java/com/herdal/dummyshoppingcenter/data/local/dao/ProductDao.kt
@@ -2,6 +2,7 @@ package com.herdal.dummyshoppingcenter.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.herdal.dummyshoppingcenter.data.local.entity.product.ProductEntity
 import kotlinx.coroutines.flow.Flow
@@ -9,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ProductDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(productEntity: ProductEntity)
 
     @Query("SELECT * FROM products")
