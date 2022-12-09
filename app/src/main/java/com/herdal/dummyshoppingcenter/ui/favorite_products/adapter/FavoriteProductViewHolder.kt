1da8ad807ba5002b06cd2d63e074b7fbe193bcf6
herdal06 <h.erdal721@gmail.com>
2022-12-09 18:31:30 +0300

    Use databinding instead of viewbinding for Favorite Product Layout

--- app/src/main/java/com/herdal/dummyshoppingcenter/ui/favorite_products/adapter/FavoriteProductViewHolder.kt
+++ app/src/main/java/com/herdal/dummyshoppingcenter/ui/favorite_products/adapter/FavoriteProductViewHolder.kt
@@ -4,18 +4,16 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.herdal.dummyshoppingcenter.databinding.ItemProductLinearBinding
 import com.herdal.dummyshoppingcenter.domain.uimodel.ProductUiModel
-import com.herdal.dummyshoppingcenter.utils.ext.getFormattedPrice
-import com.herdal.dummyshoppingcenter.utils.ext.loadImage
+import com.herdal.dummyshoppingcenter.utils.ext.executeWithAction
 
 class FavoriteProductViewHolder(
     private val binding: ItemProductLinearBinding,
     private val onFavProductClick: ((view: View, product: ProductUiModel) -> Unit)?
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(product: ProductUiModel) = binding.apply {
-        tvFavoriteProductBrand.text = product.brand
-        ivFavoriteProduct.loadImage(product.thumbnail)
-        tvFavoriteProductPrice.text = product.getFormattedPrice()
-        tvFavoriteProductTitle.text = product.title
+        binding.executeWithAction {
+            this.product = product
+        }
 
         ivFavoriteProduct.setOnClickListener {
             onFavProductClick?.invoke(ivFavoriteProduct, product)
